a5ac264430667773b239b0e73228c61a6ead6d51
rubyypal <142992214+rubyypal@users.noreply.github.com>
2023-09-26 14:10:59 +0530

    Introduction to sql and operators

---
+++ Introduction to sql.sql
@@ -0,0 +1,94 @@
+Introduction To SQL
+SQL ia an structured query language.It is a programming language used to communicate with a database
+and to perform task such as update,retrieve data from a database.
+
+what is database?
+A database is made up of a collection of tables that stores a specific set of structured data.A table is consist of rows also called as records or tuples
+and columns also called as attributes.
+
+what is datawarehouse?
+A datawarehouse is a large central location where data is managed and stored for analytical processing
+
+There are two types of database
+1.RDBMS-Relational database management system- It is a database where most of the tables are linked with each other.
+2.NOSQL-It is type of database management system that is designed to handle and store large volumes of unstructured and semi structured data
+
+what is database management system?
+A database management system is a software tool that help organize,store,retrieve data from a database and system we use to perform
+task are database management system.
+
+Different types of database management system are mysql,postgresql,sqlserver.
+
+what is a server?
+server is a software that takes request handles them and gives response.software that runs on a machine.
+
+
+DATATYPES
+There are three main DATATYPES
+1.String-char,varchar,text
+
+2.Numeric-INT,TINYINT,BIGINT,FLOAT,Real etc
+
+3.Date &Time-Date,Time,Datetime
+
+How we accumulate data everyday
+A data can be accumulated from different 'ENTITIES'
+
+An entity can be machine,person,animal.An entity can be anything that is self descriptive.
+
+Example:-A college can be the parent entity and faculty can be the child entity
+
+SQL
+Structured query language
+DDL-Data defination language
+
+DML-Data manipulation language
+
+DQL-Data query language
+
+'SELECT' COMMAND- It is used to retrieve data or information from a table
+
+Syntax for 'SELECT' statement
+
+SELECT<COLUMN>FROM<TABLE>
+
+select customername from customer;
+
+
+Operator
+What is an operator?
+It is used to perform operations in a table between two or more numbers.It allows to perform specific mathematical and logical computations on operands.
+
+There are 4 types of operators
+1. comparision operator
+2.logical operator
+3.range & membership operator
+4.airthmatic operator
+
+COMPARISION OPERATOR 
+It is a mathematical symbol allows to compare two values.
+=,!=,<=,>=
+
+Logical operator
+AND-All the condition should satisfy
+OR-Any one condition should satisfy
+
+Range & Membership operator
+IN
+
+Membership operator
+Sequence of numbers
+Between,Not Between
+
+Distinct
+It gives unique values
+
+Limit
+The limit clause is useful on large tables with thousand of records.
+
+for example-To find top 3 customer in a customer table.
+
+
+
+
+
